d940f81c9611a8a591d68709901d837ab9032d13
PedroL <pedro.lopesverardo@gmail.com>
2024-07-02 09:21:30 -0300

    DOC/add the documentation of allModels.py file

--- DL_models/allModels.py
+++ DL_models/allModels.py
@@ -2,14 +2,36 @@ from torch import nn
 import torch.nn.init as init
 import numpy as np
 
+
+'''
+The main idea of this file is to create a class for each model that we want to test.
+Each class will have the same structure, but with different number of layers and neurons in each layer.
+Testing different architectures and see which one performs better.
+'''
+
+#-------------------------------Funnel--------------------------------------------------
+'''
+The Funnel model is a model that has a decreasing number of neurons in each layer.
+Example:
+                                   (ImpL)  
+                                    /|\
+                                   n n n
+                                   \ | |
+                                     n n
+                                      \|
+                                     (out)
+ImpL -> Input Layer
+n -> Neurons
+out -> Output Layer
+'''
 class Model_Funnel1(nn.Module):
-    def __init__(self, activation, dropout_value, name: str):
+    def __init__(self, activation: nn, dropout_value: float, name: str):
         super().__init__()
         self.dropout_value = dropout_value
         self.name = name
         self.activation = activation
 
-    def inicialize_values(self, features, classes):
+    def inicialize_values(self, features: int, classes: int):
         self.inputLayer = nn.Linear(features, int(features*2))
         self.activationLayer = self.activation
         self.dropout = nn.Dropout(self.dropout_value)
@@ -28,13 +50,13 @@ class Model_Funnel1(nn.Module):
         return z
     
 class Model_Funnel2(nn.Module):
-    def __init__(self, activation, dropout_value, name: str):
+    def __init__(self, activation: nn, dropout_value: float, name: str):
         super().__init__()
         self.dropout_value = dropout_value
         self.name = name
         self.activation = activation
 
-    def inicialize_values(self, features, classes):
+    def inicialize_values(self, features: int, classes: int):
         self.inputLayer = nn.Linear(features, int(features*2))
         self.activationLayer = self.activation
         self.dropout = nn.Dropout(self.dropout_value)
@@ -56,14 +78,28 @@ class Model_Funnel2(nn.Module):
         z = self.outputLayer(z)
         return z
 #-------------------------------Straight--------------------------------------------------
+'''
+The Straight model is a model that has the same number of neurons in each layer.
+Example:
+                                       (ImpL)  
+                                        /|\
+                                       n n n
+                                       | | |
+                                       n n n
+                                       \ | /
+                                       (out)
+ImpL -> Input Layer
+n -> Neurons
+out -> Output Layer
+'''
 class Model_Straight1(nn.Module):
-    def __init__(self, activation, dropout_value, name: str):
+    def __init__(self, activation: nn, dropout_value: float, name: str):
         super().__init__()
         self.dropout_value = dropout_value
         self.name = name
         self.activation = activation
         
-    def inicialize_values(self, features, classes):
+    def inicialize_values(self, features: int, classes: int):
         self.inputLayer = nn.Linear(features, int(features))
         self.activationLayer = self.activation
         self.dropout = nn.Dropout(self.dropout_value)
@@ -88,7 +124,7 @@ class Model_Straight2(nn.Module):
         self.name = name
         self.activation = activation
     
-    def inicialize_values(self, features, classes):
+    def inicialize_values(self, features: int, classes: int):
         self.inputLayer = nn.Linear(features, int(features))
         self.activationLayer = self.activation
         self.hiddenLayer1 = nn.Linear(int(features),int((features)))
