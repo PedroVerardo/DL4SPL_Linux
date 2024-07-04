c24e6ed2f0d4ebcc8002cb41bf8f2329a63ae9b1
PedroL <pedro.lopesverardo@gmail.com>
2024-07-04 14:22:47 -0300

    add flexibility on codes

--- DL_models/modelPlots.py
+++ DL_models/modelPlots.py
@@ -7,15 +7,37 @@ import numpy as np
 from modelStatistics import ModelStatistics
 
 class ModelPlots(ModelStatistics):
-    def __init__(self, path_to_model_info: str, path_to_model: str, path_to_original_data: str, info_format: str, transpose: bool = False):
+    def __init__(self,
+                path_to_model_info: str,
+                path_to_model: str,
+                path_to_original_data: str,
+                info_format: str,
+                transpose: bool = False
+                ):
+        
         super().__init__(path_to_model_info, info_format, transpose)
         self.path_to_model = path_to_model
+
         self.original_df = self.generate_dataframe(path_to_original_data)
         self.y = self.original_df["perf"]
         self.original_df.drop(columns= ["perf", "active_options"], inplace = True)
         self.x = self.original_df
-        self.X_train , self.X_test , self.y_train , self.y_test = train_test_split(self.x, self.y, test_size=0.2, random_state=42)
-        self.X_train , self.X_val , self.y_train , self.y_val = train_test_split(self.X_train, self.y_train, test_size=0.1, random_state=42)
+
+        (self.X_train,
+         self.X_test,
+         self.y_train,
+         self.y_test) = train_test_split(self.x,
+                                         self.y,
+                                         test_size=0.2,
+                                         random_state=42)
+        
+        (self.X_train,
+         self.X_val,
+         self.y_train,
+         self.y_val) = train_test_split(self.X_train,
+                                        self.y_train,
+                                        test_size=0.1,
+                                        random_state=42)
         
         
     def plot_top_k_losses_for_optmizer(self, k:int = 5):
@@ -33,21 +55,47 @@ class ModelPlots(ModelStatistics):
     def plot_results():
         plt.figure(figsize=(20,6))
         plt.subplot(1,3,1)
-        ax = sns.lineplot(y=np.array(train_loss), x=np.array(epoch), label="loss", palette="binary")
-        ax.set(xlabel= 'epochs', ylabel= 'train_loss')
+        ax = sns.lineplot(y=np.array(train_loss),
+                          x=np.array(epoch),
+                          label="loss",
+                          palette="binary")
+        
+        ax.set(xlabel='epochs',
+               ylabel='train_loss')
+        
         plt.title("Train Loss")
         #----------------------------------------------------------------
         plt.subplot(1,3,2)
-        ax = sns.lineplot(y=np.array(train_loss), x=np.array(epoch), label="test loss", palette="flare")
-        ax.set(xlabel= 'epochs', ylabel= 'train_loss')
-        ax = sns.lineplot(x=np.array(epoch), y=np.array(validation_loss), label="Loss Convergence", color="red")
-        ax.set(xlabel= 'epochs', ylabel= 'validation_loss')
+        ax = sns.lineplot(y=np.array(train_loss),
+                          x=np.array(epoch),
+                          label="test loss",
+                          palette="flare")
+        
+        ax.set(xlabel='epochs',
+               ylabel='train_loss')
+        
+        ax = sns.lineplot(x=np.array(epoch),
+                          y=np.array(validation_loss),
+                          label="Loss Convergence",
+                          color="red")
+        
+        ax.set(xlabel='epochs',
+               ylabel='validation_loss')
+        
         plt.title("Train Vs Validation")
         #----------------------------------------------------------------
         plt.subplot(1,3,3)
-        absolut_loss = np.subtract(validation_loss, train_loss)
-        ax = sns.lineplot(x=np.array(epoch), y=absolut_loss, label="Loss Convergence", color="red")
-        ax.set(xlabel= 'epochs', ylabel= 'Vloss - Tloss')
+        absolut_loss = np.subtract(validation_loss,
+                                   train_loss)
+        
+        ax = sns.lineplot(x=np.array(epoch),
+                          y=absolut_loss,
+                          label="Loss Convergence",
+                          color="red")
+        
+        ax.set(xlabel='epochs',
+               ylabel= 'Vloss - Tloss')
+        
         plt.title("Loss Convergence")
         
     def print_df(self):
--- DL_models/plots.py
+++ DL_models/plots.py
@@ -1,28 +1,3 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-
-def plot_results(epoch, train_loss, validation_loss):
-    plt.figure(figsize=(20,6))
-    plt.subplot(1,3,1)
-    ax = sns.lineplot(y=np.array(train_loss), x=np.array(epoch), label="loss", palette="binary")
-    ax.set(xlabel= 'epochs', ylabel= 'train_loss')
-    plt.title("Train Loss")
-    #----------------------------------------------------------------
-    plt.subplot(1,3,2)
-    ax = sns.lineplot(y=np.array(train_loss), x=np.array(epoch), label="test loss", palette="flare")
-    ax.set(xlabel= 'epochs', ylabel= 'train_loss')
-    ax = sns.lineplot(x=np.array(epoch), y=np.array(validation_loss), label="Loss Convergence", color="red")
-    ax.set(xlabel= 'epochs', ylabel= 'validation_loss')
-    plt.title("Train Vs Validation")
-    #----------------------------------------------------------------
-    plt.subplot(1,3,3)
-    absolut_loss = np.subtract(validation_loss, train_loss)
-    ax = sns.lineplot(x=np.array(epoch), y=absolut_loss, label="Loss Convergence", color="red")
-    ax.set(xlabel= 'epochs', ylabel= 'Vloss - Tloss')
-    plt.title("Loss Convergence")
-
-def plot_original_vs_predicted(ytest, pred):
-    plt.plot(ytest.cpu().detach().numpy()[:100], color = 'b')
-    plt.plot(pred[:100], color = 'r', linestyle = 'dashed')
-    plt.show()
